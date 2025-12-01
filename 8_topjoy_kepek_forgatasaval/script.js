20dd1a59680f702c93d2db68372e6d72727d4b19
tanulo <tanulo@barossgabor.local>
2025-12-01 14:29:50 +0100

    8 topjoy done

--- 8_topjoy_kepek_forgatasaval/script.js
+++ 8_topjoy_kepek_forgatasaval/script.js
@@ -1,4 +1,4 @@
-const kupak = document.querySelector('.kupak');
+const kupak = document.querySelector('#kupak');
 const szoveg = document.querySelector('#szoveg');
 const tarolo = document.querySelector('.tarolo');
 
@@ -14,11 +14,32 @@ window.addEventListener('DOMContentLoaded', async () => {
         const sorok = text.split('\n').map(sor => sor.trim())
         //console.log(sorok);
 
-        sor.forEach(sor => {
+        sorok.forEach(sor => {
             uzik.push(sor);
         });
     } catch (error) {
         console.log(error);
     }
 });  
-//console.log(uzik);          
+//console.log(uzik);
+
+const kupakAnimalas =  async(start, end) => {
+    const step = start < end ? 1 : -1;
+
+    for (let i = start; i !== end + step; i +=step) {
+        await new Promise(resolve => setTimeout(resolve, 13));
+        kupak.src = `./images/bottlecap_${i}.png`;
+    };
+};
+
+tarolo.addEventListener('click', async () => {
+    if(isClosed) {
+        await kupakAnimalas(0, 10);
+        szoveg.textContent = uzik[Math.floor(Math.random() * uzik.length)]
+        isClosed = false;
+    } else {
+        szoveg.textContent = '';
+        await kupakAnimalas(10, 0);
+        isClosed = true;
+    }
+});
